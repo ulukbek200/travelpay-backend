9c922228a61ab1273d2bc1798daa666a15db74c4
ulukbek200 <144193066+ulukbek200@users.noreply.github.com>
2025-07-01 00:46:28 +0600

    fix: use index.js for json-server on Render

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+const PORT = process.env.PORT || 10000;
+
 server.use(middlewares);
 server.use(router);
-
-const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
